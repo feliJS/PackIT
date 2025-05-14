fe5b4ce38a2c2b7abb23a06551d50f02b9cca275
Felicia <190894036+feliJS@users.noreply.github.com>
2025-05-14 13:24:10 +0200

    Funktion som testar GET
    Denna koden kommer visa om GET gick igenom eller inte. (den är work in progress)

--- backend/api/users/Api-tester/script.js
+++ backend/api/users/Api-tester/script.js
@@ -7,5 +7,27 @@ function updateTestResult(testId, isSuccess, message) {
     card.classList.remove("success", "error");
     card.classList.add(isSuccess ? "success" : "error");
 
-    result.innerHTML = `<span>${message}</span>`;
+    result.innerHTML = `<pre>${message}</pre>`;
 }
+
+async function testGetAllUsers() {
+    const testId = 'test-get-users';
+    try {
+        const res = await fetch(`${url}/users`);
+        const data = await res.json();
+
+        if (res.status === 200) {
+            updateTestResult(testId, true, `Status: ${res.status}\n${JSON.stringify(data)}`);
+        } else {
+            updateTestResult(testId, false, `Unexpected response:\n${JSON.stringify(data)}`);
+        }
+    } catch (err) {
+        updateTestResult(testId, false, err.message);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#btn-test-get-users")
+        .addEventListener("click", testGetAllUsers);
+});
+

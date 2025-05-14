72aa0a966f57421176c19cced5ecf5ee94ad484a
Felicia <190894036+feliJS@users.noreply.github.com>
2025-05-14 16:15:45 +0200

    /users/:id url path fungerar!
    nu fungerar allting inom /users/:id
    dvs att man kan hämta en användare, uppdatera användaren, och även ta bort användare. (Detta ger oss möjligheter ifall vi skulle vilja ha dessa funktionerna)
    
    Först vill jag hämta id:et på användaren vilket jag gör med url.pathname.split("/")[2];, sedan också userIndex på självaste json-arrayen var användaren befinner sig.
    
    GET:
    om den ej finns med måste jag returnera det, om användaren finns så tar jag ut den exakta användaren genom getUsers[userIndex] - detta är nu med lösenord, vilket kan vara en säkerhetsrisk men det var inget vi behövde tänka på
    
    PUT:
    först måste man kolla om namnet finns, annars så är det en failad request.
    om usern inte finns alls vill vi också returnera detta i status 404.
    men! om det gick bra och vi inte hamnade på problemen ovan så kommer jag ändra just den användaren jag skrev in i bodyn. (och sedan spara ner till json filen igen)
    
    DELETE:
    delete gör samma som alla andra (repetitiv kod- not so good men detta kan enkelt delas upp i funktioner senare lite snyggare), den kommer kolla ifall indexet finns/användaren, annars returnera user not found.
    Om det gick bra däremot! ta bort 1 på indexet [userIndex] med .splice() och sedan spara ner det till json filen med den uppdaterade informationen.

--- backend/api/users/api1.js
+++ backend/api/users/api1.js
@@ -67,6 +67,45 @@ async function handler(request){
        
         }
     }
+    // /users/:id
+    if (url.pathname.startsWith("/users/")) {
+        const id = url.pathname.split("/")[2]; // Plockar ut id:et, ger: ["", "users", "abc123"]
+        const userIndex = getUsers.findIndex(user => user.id == id);
+
+        if (request.method === "GET") {
+            if (userIndex == -1) {
+                return new Response(JSON.stringify({ error: "user not found" }), { status: 404, headers: headersCORS });
+            }
+        const user = getUsers[userIndex]; //användaren vi ville få ut (m/ lösenord)
+        return new Response(JSON.stringify(user), { status: 200, headers: headersCORS }); //skicka användaren
+
+        }
+
+        if (request.method === "PUT") {
+            const body = await request.json();
+            if (!body.name) { //lägg till && body.pfp senare!
+                return new Response(JSON.stringify({ error: "Invalid request body" }), { status: 400, headers: headersCORS });
+            }
+            if (userIndex == -1) {
+                return new Response(JSON.stringify({ error: "User not found" }), { status: 404, headers: headersCORS });
+            }
+            if (body.name) {
+                getUsers[userIndex].name = body.name;
+            }
+            //if (body.pfp) {user.pfp = body.pfp;}
+            saveUsers(getUsers);
+            return new Response(JSON.stringify({ updated: "updated success" }), { status: 200, headers: headersCORS });
+        }
+    
+        if (request.method === "DELETE") {
+            if (userIndex == -1) {
+                return new Response(JSON.stringify({ error: "User not found" }), { status: 404, headers: headersCORS });
+            }
+            getUsers.splice(userIndex, 1);
+            saveUsers(getUsers);
+            return new Response(JSON.stringify({ deleted: "deleted user" }), { status: 200, headers: headersCORS });
+        }
+    }
      return new Response("Not Found", { status: 404 });
 }
 

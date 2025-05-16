f789bf7b51df31e9c6ad71410018ea9690f05284
Jenniferjepp <nelliferjj@gmail.com>
2025-05-16 13:45:09 +0200

    Test Request till väder API
    Testade att skicka en request till väder api:et och det fungerade! Jag sparade även objektet jag fick för London i en testDatabas.json för att kunna skicka requests till den istället för att använda upp de vi har tillgång till i det riktiga api:et.

--- backend/client/client1/client1.js
+++ backend/client/client1/client1.js
@@ -1,14 +1,29 @@
 
-const API_KEY_WEATHER = "";
+// Läs in API-nyckel från apiKeys.json
+const apiKeysFile = await Deno.readTextFile("../../../apiKeys.json");
+const keys = JSON.parse(apiKeysFile);
+const API_KEY_WEATHER = keys[0].API_KEY_WEATHER;
+
+/* TEST REUESTS TILL SPARAD DATA:
+const savedData = await Deno.readTextFile("./weather-london.json");
+const weather = JSON.parse(savedData);
+console.log(weather.location.localtime);
+console.log(weather.current.temperature);
+*/
+
 const BASE_URL = "https://api.weatherstack.com/current";
 
 
 // GET request - weatherObject
 export async function getWeatherDataFunc (city) {
-    const url = `${BASE_URL}?access_key=${API_KEY_WEATHER}&query=city`;
+    const url = `${BASE_URL}?access_key=${API_KEY_WEATHER}&query=${encodeURIComponent(city)}`; // måste ha "encodeURIComponent" för att servern ska kunna koda av parametern korrekt!
+    console.log("URL som skickas:", url);
 
     const response = await fetch(url);
     const weatherData = await response.json();
 
+    console.log(weatherData);
     return weatherData;
 }
+
+getWeatherDataFunc("London");

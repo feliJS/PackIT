4e06afae1abddea6d25207cfcf5af8571285836c
Jenniferjepp <nelliferjj@gmail.com>
2025-05-16 12:34:12 +0200

    Client1 (weather API) - GET req
    Async funktion skapad för att hämta väder-objektet från väder-API:et.

--- backend/client/client1/client1.js
+++ backend/client/client1/client1.js
@@ -1,1 +1,14 @@
-fetch
+
+const API_KEY_WEATHER = "";
+const BASE_URL = "https://api.weatherstack.com/current";
+
+
+// GET request - weatherObject
+export async function getWeatherDataFunc (city) {
+    const url = `${BASE_URL}?access_key=${API_KEY_WEATHER}&query=city`;
+
+    const response = await fetch(url);
+    const weatherData = await response.json();
+
+    return weatherData;
+}

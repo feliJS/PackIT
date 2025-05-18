2993ba552deda67a95514abd593dfcfc04d6670f
tiffanystud <tiffanylarsson.stud@gmail.com>
2025-05-18 21:17:18 +0200

    Main server setup
    Grunden satt för main servern.
    
    Den servar de befintliga statiska filerna (css, bilder, ikoner).

--- server.js
+++ server.js
@@ -1,15 +1,78 @@
-// Main server
-import { serveFile, serveDir } from "jsr:@std/http";
 
-function serverHandler(request) {
-    const url = new URL(request.url);
-    if (url.pathname == "/common") {
-        return serveFile(request, "frontend/common/common.html");
-    } else if(url.pathname.startsWith("/common")) {
-        return serveDir(request, { fsRoot: "frontend" });  
+// Main server
+
+import { serveFile } from "jsr:@std/http";
+
+async function serverHandler(request) {
+
+    const reqUrl = new URL(request.url);
+    const reqPathname = reqUrl.pathname;
+    console.log("Begärrd path: " + reqPathname);
+
+
+    if (reqPathname === "/common/common.css") {
+        return await serveFile(request, "frontend/common/common.css");
     }
 
-    return serveDir(request, { fsRoot: "." });  
+
+    if (reqPathname.startsWith("/icons/")) {
+        const filePath = `frontend${reqPathname}`;
+        try {
+            return await serveFile(request, filePath);
+        } catch {
+            return new Response("Not Found", { status: 404 });
+        }
+    }
+
+
+    if (reqPathname.startsWith("/images/")) {
+        const filePath = `frontend${reqPathname}`;
+        try {
+            return await serveFile(request, filePath);
+        } catch {
+            return new Response("Not Found", { status: 404 });
+        }
+    }
+
+
+    if (reqPathname.startsWith("/common/")) {
+        const filePath = `frontend${reqPathname}`;
+        try {
+            return await serveFile(request, filePath);
+        } catch {
+            return new Response("Not Found", { status: 404 });
+        }
+    }
+
+
+    if (reqPathname.startsWith("/index")) {
+        return await serveFile(request, "frontend/pagesIndex/index.html");
+    }
+
+
+    if (reqPathname.startsWith("/profile")) {
+        return await serveFile(request, "frontend/pagesProfile/index.html");
+    }
+
+
+    if (reqPathname.startsWith("/create-list")) {
+        return await serveFile(request, "frontend/pagesCreateList/index.html");
+    }
+
+
+    if (reqPathname === "/" || reqPathname === "/home") {
+        return await serveFile(request, "frontend/pagesIndex/index.html");
+    }
+
+
+    // Fallback
+    return new Response("The page could not be found", {
+        status: 404,
+        headers: {
+            "Content-Type": "text/plain",
+        },
+    });
+
 }
 
-Deno.serve({ port: 4242 }, serverHandler);
+Deno.serve(serverHandler);

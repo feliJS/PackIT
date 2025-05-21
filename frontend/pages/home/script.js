8d501233f30184b6041fd23bc3227405eabeebde
Felicia <190894036+feliJS@users.noreply.github.com>
2025-05-21 10:01:14 +0200

    'hidden' på Login o Register box!
    Nu ser man ej boxen om att logga in och regristrera sig om man har loggat in och regristrerat sig!

--- frontend/pages/home/script.js
+++ frontend/pages/home/script.js
@@ -1,20 +1,42 @@
 function getCookie(name) { //hittar rätt cookie
     return document.cookie
         .split('; ')
-        .find(cookie => cookie.startsWith(name + '='))
+        .find(cookie => cookie.startsWith(name + "="))
         ?.split('=')[1] || null;
 }
 
-const sessionId = getCookie('session_id');
+async function main() {
+    const sessionId = getCookie("session_id");
+    const userData = await getSpecificUser(sessionId); 
 
-const createListButton = document.getElementById('create-list-button');
-const errorMsg = document.getElementById('error-create-list');
+    const loginDiv = document.getElementById("log-in-div");
+    const createListButton = document.getElementById("create-list-button");
+    const errorMsg = document.getElementById("error-create-list");
+    const header = document.querySelector("#create-list-div h1");
 
-createListButton.addEventListener('click', function (e) {
-    if (!sessionId) {
-        errorMsg.textContent = "You need to login or create account before creating a list!";
-    } else {
-        errorMsg.textContent = "";
-        //här kommer sedan en "redirect till koden som kommer öppna frågorna
+    createListButton.addEventListener("click", function (e) {
+        if (!sessionId) {
+            errorMsg.textContent = "You need to login or create an account before creating a list!";
+        } else {
+            errorMsg.textContent = "";
+            //här kommer sedan en "redirect till koden som kommer öppna frågorna
+        }
+    });
+
+    if (sessionId) {
+        if (loginDiv) {
+            loginDiv.style.visibility = 'hidden';
+        }
+        if (userData && userData.name) {
+            header.textContent = `Welcome, ${userData.name}!`;
+        } 
     }
-});
+    else{
+        if (loginDiv) {
+            loginDiv.style.visibility = 'visible';
+            header.textContent = "CREATE YOUR PACKING LIST";
+        }
+    }
+}
+
+main();

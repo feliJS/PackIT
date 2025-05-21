cd22463e528ab50a3acb1210ad2eea67b7a6f856
Felicia <190894036+feliJS@users.noreply.github.com>
2025-05-21 11:18:16 +0200

    Profil rutan-
    syns bara i registrera och inte i log in

--- frontend/common/common.js
+++ frontend/common/common.js
@@ -1,17 +1,18 @@
-
 //register & login
 import { UserAPI } from '/common/client-class.js';
 const userApi = new UserAPI('http://localhost:8000');
 
-function createAccountPopup(containerSelector, title, btnId, btnText, onBtnClick) {
+function createAccountPopup(containerSelector, btnId, btnText, onBtnClick, showProfilePic) {
     const container = document.querySelector(containerSelector);
     if (!container) return;
+
+    // Lägg bara till profile-pic om showProfilePic är true
+    const profilePicHTML = showProfilePic ? '<div class="profile-pic"></div>' : '';
 
     container.innerHTML = `
         <div class="account-popup" id="account-field">
             <div class="account-container">
-                <div class="profile-pic"></div>
-                <h1>${title}</h1>
+                ${profilePicHTML}
                 <div class="inputs">
                     <input id="username" placeholder="Username" type="text">
                     <input id="password" placeholder="Password" type="password">
@@ -25,11 +26,11 @@ function createAccountPopup(containerSelector, title, btnId, btnText, onBtnClick
     const cancelBtn = document.getElementById('cancel-btn');
     const originalText = cancelBtn.textContent;
 
-    cancelBtn.addEventListener('mouseenter', function() {
+    cancelBtn.addEventListener('mouseenter', () => {
         cancelBtn.textContent = originalText + ' :(';
     });
 
-    cancelBtn.addEventListener('mouseleave', function() {
+    cancelBtn.addEventListener('mouseleave', () => {
         cancelBtn.textContent = originalText;
     });
 
@@ -42,29 +43,28 @@ function createAccountPopup(containerSelector, title, btnId, btnText, onBtnClick
 function createRegister() {
     createAccountPopup(
         '.register-box',
-        'Create account!',
         'reg',
         'Register!',
-        function() {
+        () => {
             const username = document.getElementById("username").value;
             const password = document.getElementById("password").value;
             userApi.newAccount(username, password).then(user => {
                 close();
-                window.location.reload(true); //inte det bästa sättet att ladda om.. kolla gärna på bättre!
+                window.location.reload(true);
             }).catch(err => {
                 alert("Kunde inte skapa konto! " + (err.message || err));
             });
-        }
+        },
+        true 
     );
 }
 
 function createLogin() {
     createAccountPopup(
         '.login-box',
-        'Login!',
         'log',
         'Login!',
-        function() {
+        () => {
             const username = document.getElementById("username").value;
             const password = document.getElementById("password").value;
             userApi.loginUser(username, password).then(user => {
@@ -73,7 +73,8 @@ function createLogin() {
             }).catch(err => {
                 alert("Kunde ej logga in! " + (err.message || err));
             });
-        }
+        },
+        false 
     );
 }
 
@@ -86,10 +87,10 @@ function openLogin() {
 }
 
 function close() {
-   const regBox = document.querySelector(".register-box");
-   if (regBox) regBox.innerHTML = "";
-   const loginBox = document.querySelector(".login-box");
-   if (loginBox) loginBox.innerHTML = "";
+    const regBox = document.querySelector(".register-box");
+    if (regBox) regBox.innerHTML = "";
+    const loginBox = document.querySelector(".login-box");
+    if (loginBox) loginBox.innerHTML = "";
 }
 
 document.getElementById("create-acc-button").addEventListener('click', openRegister);

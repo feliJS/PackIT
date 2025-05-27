9db0d221df7f24e1281f7c2785061e38aed63d82
Jenniferjepp <nelliferjj@gmail.com>
2025-05-27 11:58:00 +0200

    Update weather-client OFFLINE
    ändrat hur man skickar förfrågningar offline

--- frontend/client/weater-client.js
+++ frontend/client/weater-client.js
@@ -1,4 +1,65 @@
 
+const weatherDB = [
+    {
+        "request": {
+          "type": "City",
+          "query": "London, United Kingdom",
+          "language": "en",
+          "unit": "m"
+        },
+        "location": {
+          "name": "London",
+          "country": "United Kingdom",
+          "region": "City of London, Greater London",
+          "lat": "51.517",
+          "lon": "-0.106",
+          "timezone_id": "Europe/London",
+          "localtime": "2025-05-16 12:31",
+          "localtime_epoch": 1747398660,
+          "utc_offset": "1.0"
+        },
+        "current": {
+          "observation_time": "11:31 AM",
+          "temperature": 17,
+          "weather_code": 113,
+          "weather_icons": [
+            "https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0001_sunny.png"
+          ],
+          "weather_descriptions": [ "Sunny" ],
+          "astro": {
+            "sunrise": "05:07 AM",
+            "sunset": "08:48 PM",
+            "moonrise": "12:26 AM",
+            "moonset": "06:55 AM",
+            "moon_phase": "Waning Gibbous",
+            "moon_illumination": 90
+          },
+          "air_quality": {
+            "co": "270.1",
+            "no2": "12.395",
+            "o3": "87",
+            "so2": "2.59",
+            "pm2_5": "9.805",
+            "pm10": "17.02",
+            "us-epa-index": "1",
+            "gb-defra-index": "1"
+          },
+          "wind_speed": 19,
+          "wind_degree": 10,
+          "wind_dir": "N",
+          "pressure": 1026,
+          "precip": 0,
+          "humidity": 42,
+          "cloudcover": 0,
+          "feelslike": 17,
+          "uv_index": 6,
+          "visibility": 10,
+          "is_day": "yes"
+        }
+      }
+]
+
+
 // Läs in API-nyckel från apiKeys.json
 /* const apiKeysFile = await Deno.readTextFile("../../../apiKeys.json");
 const keys = JSON.parse(apiKeysFile);
@@ -9,10 +70,11 @@ const BASE_URL = "https://api.weatherstack.com/current";
 
 // Klass som innehåller nycklarna temperature, localTime och country
 class WeatherData {
-    constructor(temperature, localTime, country) {
+    constructor(temperature, localTime, country, weatherDescriptions) {
         this.temperature = temperature;
         this.localTime = localTime;
         this.country = country;
+        this.weatherDescriptions = weatherDescriptions;
     }
 }
 
@@ -20,8 +82,7 @@ class WeatherData {
 // GET request - weatherObject
 export async function getWeatherDataFunc (city) {
     // TEST REQUESTS TILL SPARAD DATA:
-    const testDatabasFile = await Deno.readTextFile("testDatabas.json");
-    const testWeatherObj = JSON.parse(testDatabasFile)[0];
+    
 
     // REQUESTS ONLINE
     // const url = `${BASE_URL}?access_key=${API_KEY_WEATHER}&query=${encodeURIComponent(city)}`; // måste ha "encodeURIComponent" för att servern ska kunna koda av parametern korrekt!
@@ -34,7 +95,7 @@ export async function getWeatherDataFunc (city) {
     // för online:
     // const weather = new WeatherData(weatherObject.current.temperature, weatherObject.location.localtime, weatherObject.location.country);
     // för offline:
-    const weather = new WeatherData(testWeatherObj.current.temperature, testWeatherObj.location.localtime, testWeatherObj.location.country);
+    const weather = new WeatherData(testWeatherObj.current.temperature, testWeatherObj.location.localtime, testWeatherObj.location.country, testWeatherObj.current.weather_descriptions[0]);
 
     // console.log(weatherObject);
     console.log(weather);

59c98db44dc64b57e7382f0a290c6282b5dc76c5
Felicia <190894036+feliJS@users.noreply.github.com>
2025-05-27 14:53:57 +0200

    Update api.js

--- backend/api/api.js
+++ backend/api/api.js
@@ -4,18 +4,22 @@ import { imageHandler } from "./unsplash/imageApi.js";
 
 function handler(req) {
     let url = new URL(req.url);
-if(url.pathname.startsWith("/users")) {
-    let usersResponse = usersHandler(req);
-    if(usersResponse) return usersResponse;
 
-    let listResponse = listHandler(req);
-    if(listResponse) return listResponse;
-}
-if(url.pathname.startsWith("/image")) {
-    let imageResponse = imageHandler(req);
-    if(imageResponse) return imageResponse;
-}
-return null
+    if(url.pathname.startsWith("/users")) {
+        let usersResponse = usersHandler(req);
+        if(usersResponse) return usersResponse;
+    }
+
+    if(url.pathname.startsWith("/lists")){
+        let listResponse = listHandler(req);
+        if(listResponse) return listResponse;
+    }
+
+    if(url.pathname.startsWith("/image")) {
+        let imageResponse = imageHandler(req);
+        if(imageResponse) return imageResponse;
+    }
+    return null
 }
 
 Deno.serve(handler);

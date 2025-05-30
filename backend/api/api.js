0379d9c142053d0769eadb964dcab5cc3f7c0b71
Felicia <190894036+feliJS@users.noreply.github.com>
2025-05-31 00:51:37 +0200

    /weather som en endpoint istället
    Detta är för att apikeys brukar hanteras i backend och inte i frontend, man kan ta ut keyn i backend. Nu är det som vi skickar en request till vår egna endpoint och server och sen skickar till väder apiet med det vilket är en lösning som finns!

--- backend/api/api.js
+++ backend/api/api.js
@@ -2,7 +2,7 @@ import { usersHandler } from "./users/users-api.js";
 import { listHandler } from "./lists/list-api.js";
 import { imageHandler } from "./unsplash/imageApi.js";
 
-function handler(req) {
+async function handler(req) {
     let url = new URL(req.url);
 
     if(url.pathname.startsWith("/users")) {
@@ -19,6 +19,18 @@ function handler(req) {
         let imageResponse = imageHandler(req);
         if(imageResponse) return imageResponse;
     }
+
+    if(url.pathname == "/weather") {
+        const apiKeysFile = await Deno.readTextFile("../../apiKeys.json");
+        const keys = JSON.parse(apiKeysFile);
+        const API_KEY_WEATHER = keys[0].API_KEY_WEATHER; 
+        const BASE_URL = "https://api.weatherstack.com/current";
+        let body = await req.json();
+        let res = await fetch(`${BASE_URL}?access_key=${API_KEY_WEATHER}&query=${encodeURIComponent(body.city)}`);
+        let data = await res.json();
+        return new Response(JSON.stringify(data), { headers: { "Access-Control-Allow-Origin": "*", "content-type": "application/json" } })
+    } 
+
     return null
 }
 
